f60cbfb8abd150573b2eab182972e214d18ddf2a
kandurisripaada <113902793+kandurisripaada@users.noreply.github.com>
2025-03-27 10:34:52 +0530

    Update install.py

--- attrition-retention-main/install.py
+++ attrition-retention-main/install.py
@@ -1,5 +1,5 @@
 import os
 
 print("Installing dependencies from requirements.txt...")
-os.system("pip install -r requirements.txt")
+os.system("pip install --upgrade pip && pip install -r requirements.txt")
 print("Dependencies installed successfully!")
