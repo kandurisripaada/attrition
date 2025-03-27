89bb49d0d8f73a5759b98e6b852ac0b5e7cba5b4
kandurisripaada <113902793+kandurisripaada@users.noreply.github.com>
2025-03-27 10:50:07 +0530

    Delete attrition-retention-main directory

--- attrition-retention-main/attrition-retention-main/install.py
+++
@@ -1,5 +0,0 @@
-import os
-
-print("Installing dependencies from requirements.txt...")
-os.system("pip install -r requirements.txt")
-print("Dependencies installed successfully!")
--- attrition-retention-main/install.py
+++
@@ -1,5 +0,0 @@
-import os
-
-print("Installing dependencies from requirements.txt...")
-os.system("pip install --upgrade pip && pip install -r requirements.txt")
-print("Dependencies installed successfully!")
